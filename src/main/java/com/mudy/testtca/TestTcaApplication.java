8429b5fec0a48c9497185ee7eff8f6efce1f9123
CoderMudy <2808050775@qq.com>
2022-07-29 23:26:16 +0800

    first

---
+++ src/main/java/com/mudy/testtca/TestTcaApplication.java
@@ -0,0 +1,13 @@
+package com.mudy.testtca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTcaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTcaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mudy/testtca/TestTcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudy.testtca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTcaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
